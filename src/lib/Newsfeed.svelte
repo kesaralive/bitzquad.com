ab45861a6cd95f23e2507d93335c0a1ffafd75c9
hariwij <hariwijesinghe234@gmail.com>
2022-01-05 13:37:14 +0000

    hover comp

--- src/lib/Newsfeed.svelte
+++ src/lib/Newsfeed.svelte
@@ -1,9 +1,10 @@
 <div class="container mx-auto mt-10" style="width:100% height:50vh">
     <div
-        class="grid grid-cols-3 grid-rows-4 gap-4 sm:overflow-x-auto"
-        style="width: 100%; height:100%;"
+        class="grid grid-cols-3 grid-rows-4 gap-10 sm:overflow-x-auto overflow-visible"
+        style="width: 100%; height:100%; z-index:-1;"
     >
         <div class="newsCard row-span-2">
+            <div class="date" ><h4>18</h4></div>
             <div class="imgContainer1">
                 <img src="/mongodb-icon.svg" alt="" />
             </div>
@@ -62,7 +63,7 @@
         @apply justify-start;
     }
     .newsCard {
-        @apply border-2 border-slate-600 rounded-lg shadow-md;
+        @apply border-2 border-slate-600 rounded-lg shadow-md relative hover:scale-105 hover:duration-500;
         min-height: 80px;
         width: 100%;
     }
@@ -70,8 +71,14 @@
         @apply border-t-2 border-slate-600 text-center p-3;
         width: 100%;
         height: 100%;
+        z-index: 1;
     }
     .newsCard div img {
         height: 150px;
+    }
+    .date{
+        @apply absolute top-0 left-0 rounded-full bg-slate-600 h-32 w-32 hover:h-72 hover:w-72 hover:duration-500;
+        transform: translate(-50%,-50%);
+        z-index: 100;
     }
 </style>
