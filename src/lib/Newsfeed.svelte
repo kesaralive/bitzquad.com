c2c05bfffc8e9b494cd269e5af11dcb5812a1b64
hariwij <hariwijesinghe234@gmail.com>
2022-01-05 08:35:14 +0000

    sample 01

--- src/lib/Newsfeed.svelte
+++ src/lib/Newsfeed.svelte
@@ -4,43 +4,74 @@
         style="width: 100%; height:100%;"
     >
         <div class="newsCard row-span-2">
-            <div><img src="/mongodb-icon.svg" alt="" /></div>
-            <div class="newsContent"><h2>Title</h2></div>
+            <div class="imgContainer1">
+                <img src="/mongodb-icon.svg" alt="" />
+            </div>
+            <div class="newsContent">
+                <h2>Can coffee make you a better developer?</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                    exercitationem praesentium nihil.
+                </p>
+            </div>
         </div>
         <div class="newsCard col-span-2">
-            <div><img src="/mongodb-icon.svg" alt="" /></div>
+            <div class="imgContainer2">
+                <img src="/mongodb-icon.svg" alt="" />
+            </div>
         </div>
         <div class="newsCard col-span-2">
-            <div><img src="/mongodb-icon.svg" alt="" /></div>
+            <div class="imgContainer2">
+                <img src="/mongodb-icon.svg" alt="" />
+            </div>
         </div>
         <div class="newsCard col-span-2">
-            <div><img src="/mongodb-icon.svg" alt="" /></div>
+            <div class="imgContainer2">
+                <img src="/mongodb-icon.svg" alt="" />
+            </div>
         </div>
         <div class="newsCard row-span-2">
-            <div><img src="/mongodb-icon.svg" alt="" /></div>
+            <div class="imgContainer1">
+                <img src="/mongodb-icon.svg" alt="" />
+            </div>
         </div>
         <div class="newsCard col-span-2">
-            <div><img src="/mongodb-icon.svg" alt="" /></div>
+            <div class="imgContainer2">
+                <img src="/mongodb-icon.svg" alt="" />
+            </div>
         </div>
     </div>
 </div>
 
 <style>
+    h2 {
+        @apply text-lg font-semibold;
+    }
+    p{
+        @apply text-gray-600;
+    }
+    .imgContainer1 {
+        @apply justify-center;
+    }
+    .imgContainer1 img {
+        @apply justify-center;
+        width: 100%;
+    }
+    .imgContainer1 {
+        @apply justify-start;
+    }
     .newsCard {
-        @apply border-2 border-slate-600 rounded-lg text-center;
+        @apply border-2 border-slate-600 rounded-lg shadow-md;
         min-height: 80px;
         width: 100%;
     }
     .newsCard .newsContent {
-        @apply border-t-2 border-slate-600 justify-start;
-        width:100%;
+        @apply border-t-2 border-slate-600 text-center p-3;
+        width: 100%;
         height: 100%;
     }
-    .newsCard .row-span-2 div {
-        @apply flex justify-center;
-    }
     .newsCard div img {
-        width: 100%;
         height: 150px;
     }
 </style>
